e721bafad0f5d9eb6a93b4e8158a6c47e0dad1b3
srividyakun <srividya.ece@gmail.com>
2020-02-16 15:58:28 +0100

    firstcommit

--- src/testcases/ReverseString.java
+++ src/testcases/ReverseString.java
@@ -7,19 +7,20 @@ public class ReverseString {
 		//Reverse a String: 
 		//diff bw String and StringBuffer
 		//do we have reverse function in String?
-		
+		//new line
+
 		String s = "Hello World";
-		
+
 		//1. using for loop 
 		int len = s.length(); //8
 		String rev = "";
-		
-		for(int i =len-1; i>=0; i--){
+
+		for (int i = len - 1; i >= 0; i--) {
 			rev = rev + s.charAt(i); //muineleS
 		}
-		
+
 		System.out.println(rev);
-		
+
 		//2. using StringBuffer class:
 		StringBuffer sf = new StringBuffer(s);
 		System.out.println(sf.reverse());
