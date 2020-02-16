c79ea1696c0967d866e4c6bce4b103e87abf35d9
srividyakun <srividya.ece@gmail.com>
2020-02-16 16:00:45 +0100

    firstcommit

--- src/testcases/Base.java
+++ src/testcases/Base.java
@@ -1,14 +1,13 @@
 package testcases;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class Base {
@@ -16,7 +15,7 @@ public class Base {
 	public static WebDriver driver;
 
 	public static void initialization() {
-
+		//this is new line
 		System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");
 		driver = new ChromeDriver();
 
